3c94ac80c1d085d3645be54c44c4a274c16f12ab
Flampt <60038323+FlamptX@users.noreply.github.com>
2021-05-07 13:57:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 from discord.ext import commands
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
 
 bot = commands.Bot(command_prefix='!')
 
 bot.load_extension("Makeemoji")
 
-bot.run(TOKEN)
+bot.run("token")
