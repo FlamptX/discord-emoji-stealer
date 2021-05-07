2266db4c151ccff91d93aa2028c5836f4f8244f4
Flampt <60038323+FlamptX@users.noreply.github.com>
2021-05-07 16:20:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
-bot.load_extension("Makeemoji")
+bot.load_extension("cogs.Makeemoji")
+bot.load_extension("cogs.StealEmoji")
 
 bot.run("token")
