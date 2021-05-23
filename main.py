3fe5e796ef4a9e33868138aa7850100483d9332c
Flampt <60038323+FlamptX@users.noreply.github.com>
2021-05-23 18:49:50 +0200

    Update and rename test.py to main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ bot = commands.Bot(command_prefix='!')
 
 bot.load_extension("cogs.Makeemoji")
 bot.load_extension("cogs.StealEmoji")
-bot.load_extension("cogs.Putemoji")
-bot.load_extension("cogs.Fastput")
+bot.load_extension("cogs.AddEmoji")
+
 bot.run("token")
