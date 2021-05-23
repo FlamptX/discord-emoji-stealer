62ea46245fd2d27b39724c84e2d2ddada1ecc55e
Flampt <60038323+FlamptX@users.noreply.github.com>
2021-05-23 13:26:45 +0200

    Update Makeemoji.py

--- cogs/Makeemoji.py
+++ cogs/Makeemoji.py
@@ -10,31 +10,28 @@ class Makeemoji(commands.Cog):
     async def makeemoji(self, ctx, name, url=None):
         if url:
             async with aiohttp.ClientSession() as session:
-             async with session.get(url, allow_redirects=True) as resp:
-              file_request = await resp.read()
+                async with session.get(url, allow_redirects=True) as resp:
+                    file_request = await resp.read()
             try:
-                emoji = await ctx.guild.create_custom_emoji(image=file_request.content, name=name)
+                emoji = await ctx.guild.create_custom_emoji(image=file_request, name=name)
                 await ctx.send(f"Emoji <:{emoji.name}:{emoji.id}> was created!")
             except discord.InvalidArgument:
                 await ctx.send("You must attach an **image** or a **gif** for the emoji, not a different type of the file.")
             return
         try:
-            attachment_url = ctx.message.attachments[0].url
+            url = ctx.message.attachments[0].url
         except IndexError:
             await ctx.send("You must attach an image or a gif for the emoji.")
             return
         async with aiohttp.ClientSession() as session:
-          async with session.get(url, allow_redirects=True) as resp:
-           file_request = await resp.read()
+            async with session.get(url, allow_redirects=True) as resp:
+                file_request = await resp.read()
         try:
-            emoji = await ctx.guild.create_custom_emoji(image=file_request.content, name=name)
+            emoji = await ctx.guild.create_custom_emoji(image=file_request, name=name)
         except discord.InvalidArgument:
             await ctx.send("You must attach an **image** or a **gif** for the emoji, not a different type of the file.")
             return
-        if emoji.animated:
-            await ctx.send(f"Emoji <a:{emoji.name}:{emoji.id}> was created!")
-        elif not emoji.animated:
-            await ctx.send(f"Emoji <:{emoji.name}:{emoji.id}> was created!")
+        await ctx.send(f"Emoji <:{emoji.name}:{emoji.id}> was created!")
 
     @makeemoji.error
     async def makeemoji_error(self, ctx, error):
