0daf323071461c67da2ce763609e386e041947b3
Flampt <60038323+FlamptX@users.noreply.github.com>
2021-05-23 18:52:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
-bot.load_extension("cogs.Makeemoji")
+bot.load_extension("cogs.MakeEmoji")
 bot.load_extension("cogs.StealEmoji")
 bot.load_extension("cogs.AddEmoji")
 
